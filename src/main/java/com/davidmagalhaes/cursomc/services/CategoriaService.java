e608227ec8646872fbe5e0d4c482fe825ff0d72c
David Magalhães <deivi_gf144@hotmail.com>
2022-01-01 20:37:12 -0400

    Listando todas categorias com DTO

--- src/main/java/com/davidmagalhaes/cursomc/services/CategoriaService.java
+++ src/main/java/com/davidmagalhaes/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.davidmagalhaes.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +45,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos");
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
