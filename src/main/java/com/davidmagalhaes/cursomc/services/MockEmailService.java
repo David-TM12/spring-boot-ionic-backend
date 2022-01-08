522883bd9f65a9ccbde5a1ad2c0765250ea5a2b8
David Magalhães <deivi_gf144@hotmail.com>
2022-01-08 12:43:56 -0400

    revertendo commit anterior

--- src/main/java/com/davidmagalhaes/cursomc/services/MockEmailService.java
+++ src/main/java/com/davidmagalhaes/cursomc/services/MockEmailService.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
-@Service
 public class MockEmailService extends AbstractEmailService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
--- src/main/java/com/davidmagalhaes/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/davidmagalhaes/cursomc/services/SmtpEmailService.java
@@ -7,7 +7,6 @@ import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
-@Service
 public class SmtpEmailService extends AbstractEmailService{
 
 	@Autowired
