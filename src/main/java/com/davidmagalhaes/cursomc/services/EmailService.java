e9c5d1957040b6b093bfb52f3601c343adca7ae4
David Magalhães <deivi_gf144@hotmail.com>
2022-01-08 12:53:35 -0400

    Adicionando anotação @Service no serviço de EmailService

--- src/main/java/com/davidmagalhaes/cursomc/services/EmailService.java
+++ src/main/java/com/davidmagalhaes/cursomc/services/EmailService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.davidmagalhaes.cursomc.domain.Pedido;
 
-
+@Service
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
