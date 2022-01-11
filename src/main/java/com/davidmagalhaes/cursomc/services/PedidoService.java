8e1f2ec5d5b0b5d0b37329299df0d361613718da
david <deivi_gf144@hotmail.com>
2022-01-11 16:48:33 -0400

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/davidmagalhaes/cursomc/services/PedidoService.java
+++ src/main/java/com/davidmagalhaes/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.davidmagalhaes.cursomc.domain.Cliente;
 import com.davidmagalhaes.cursomc.domain.ItemPedido;
 import com.davidmagalhaes.cursomc.domain.PagamentoComBoleto;
 import com.davidmagalhaes.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.davidmagalhaes.cursomc.domain.enums.EstadoPagamento;
 import com.davidmagalhaes.cursomc.repositories.ItemPedidoRepository;
 import com.davidmagalhaes.cursomc.repositories.PagamentoRepository;
 import com.davidmagalhaes.cursomc.repositories.PedidoRepository;
+import com.davidmagalhaes.cursomc.security.UserSS;
+import com.davidmagalhaes.cursomc.services.exceptions.AuthorizationException;
 import com.davidmagalhaes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,6 +75,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user ==null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
