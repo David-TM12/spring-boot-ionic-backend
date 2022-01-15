fec33e54a44f75bef3f09de97bde95d24fcae691
David Magalhães <deivi_gf144@hotmail.com>
2022-01-15 12:11:53 -0400

    Salvando primeiro arquivo no S3

--- src/main/java/com/davidmagalhaes/cursomc/CursomcApplication.java
+++ src/main/java/com/davidmagalhaes/cursomc/CursomcApplication.java
@@ -1,39 +1,17 @@
 package com.davidmagalhaes.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.davidmagalhaes.cursomc.domain.Categoria;
-import com.davidmagalhaes.cursomc.domain.Cidade;
-import com.davidmagalhaes.cursomc.domain.Cliente;
-import com.davidmagalhaes.cursomc.domain.Endereco;
-import com.davidmagalhaes.cursomc.domain.Estado;
-import com.davidmagalhaes.cursomc.domain.ItemPedido;
-import com.davidmagalhaes.cursomc.domain.Pagamento;
-import com.davidmagalhaes.cursomc.domain.PagamentoComBoleto;
-import com.davidmagalhaes.cursomc.domain.PagamentoComCartao;
-import com.davidmagalhaes.cursomc.domain.Pedido;
-import com.davidmagalhaes.cursomc.domain.Produto;
-import com.davidmagalhaes.cursomc.domain.enums.EstadoPagamento;
-import com.davidmagalhaes.cursomc.domain.enums.TipoCliente;
-import com.davidmagalhaes.cursomc.repositories.CategoriaRepository;
-import com.davidmagalhaes.cursomc.repositories.CidadeRepository;
-import com.davidmagalhaes.cursomc.repositories.ClienteRepository;
-import com.davidmagalhaes.cursomc.repositories.EnderecoRepository;
-import com.davidmagalhaes.cursomc.repositories.EstadoRepository;
-import com.davidmagalhaes.cursomc.repositories.ItemPedidoRepository;
-import com.davidmagalhaes.cursomc.repositories.PagamentoRepository;
-import com.davidmagalhaes.cursomc.repositories.PedidoRepository;
-import com.davidmagalhaes.cursomc.repositories.ProdutoRepository;
+import com.davidmagalhaes.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -42,7 +20,7 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {	
 		
-		
+		s3Service.uploadFile("/home/davidtm/Imagens/Tabela.png");
 	}
 
 }
