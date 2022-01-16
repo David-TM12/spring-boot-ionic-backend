a86821bc162173d8dd301c430eec18732ac0eec9
David Magalhães <deivi_gf144@hotmail.com>
2022-01-16 13:12:10 -0400

    Salvando a URL da imagem em Cliente

--- src/main/java/com/davidmagalhaes/cursomc/services/ClienteService.java
+++ src/main/java/com/davidmagalhaes/cursomc/services/ClienteService.java
@@ -117,6 +117,17 @@ public class ClienteService {
 	}
 
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		
+		UserSS user = UserService.authenticated();
+		
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		URI uri = s3Service.uploadFile(multipartFile);
+		
+		Cliente cli = repo.findById(user.getId()).get();
+		cli.setImageUrl(uri.toString());
+		repo.save(cli);
+		return uri;
 	}
 }
