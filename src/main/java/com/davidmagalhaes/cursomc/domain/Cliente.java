aeb468bee7a621d26d05208b78c0c26fb0537340
David Magalhães <deivi_gf144@hotmail.com>
2022-01-16 16:22:39 -0400

    Usando padrão de nomes para imagens

--- src/main/java/com/davidmagalhaes/cursomc/domain/Cliente.java
+++ src/main/java/com/davidmagalhaes/cursomc/domain/Cliente.java
@@ -53,9 +53,7 @@ public class Cliente implements Serializable{
 	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-	
-	private String imageUrl;
-	
+
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -176,13 +174,5 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
+	
 }
