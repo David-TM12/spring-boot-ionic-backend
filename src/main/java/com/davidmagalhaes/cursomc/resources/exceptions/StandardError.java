cf1dd08ab0dbbf286da51f6bb76a499cdf6a06d3
David Magalhães <deivi_gf144@hotmail.com>
2022-01-18 20:48:45 -0400

    Padronizando formato de excecoes

--- src/main/java/com/davidmagalhaes/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/davidmagalhaes/cursomc/resources/exceptions/StandardError.java
@@ -5,39 +5,52 @@ import java.io.Serializable;
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private Integer status;
-	private String msg;
 	private Long timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
 	
-	public StandardError(Integer status, String msg, Long timestamp) {
+	public StandardError(Long timestamp, Integer status, String error, String message, String path) {
 		super();
-		this.status = status;
-		this.msg = msg;
 		this.timestamp = timestamp;
-	}
-
-	public Integer getStatus() {
-		return status;
-	}
-
-	public void setStatus(Integer status) {
 		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
 	}
-
-	public String getMsg() {
-		return msg;
-	}
-
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-
+	
 	public Long getTimestamp() {
 		return timestamp;
 	}
-
 	public void setTimestamp(Long timestamp) {
 		this.timestamp = timestamp;
 	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
 	
 }
